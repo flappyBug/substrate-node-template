05cddc6ee44dcbeb2c01c51d507f9c0dfc013840
Haoran Lin <tdkgn@outlook.com>
2023-06-08 16:32:24 +0800

    make test pass

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,14 +1,29 @@
 use crate::{mock::*, Error, Event};
-use frame_support::{assert_noop, assert_ok};
+use frame_support::{assert_noop, assert_ok, traits::Currency};
+use pallet_balances::Error as BalanceError;
+use sp_runtime::traits::AccountIdConversion;
+
+fn assert_balance(account_id: u64, balance: u128) {
+	assert_eq!(Balances::total_balance(&account_id), balance);
+}
 
 #[test]
 fn create_kitty_works() {
 	new_test_ext().execute_with(|| {
 		let kitty_id = 0;
 		let account_id = 1;
-
+		let node_account = AccountIdConversion::<u64>::into_account_truncating(&PALLET_ID);
 		assert_eq!(KittiesModule::next_kitty_id(), kitty_id);
+		assert_noop!(
+			KittiesModule::create(RuntimeOrigin::signed(account_id)),
+			BalanceError::<Test>::InsufficientBalance
+		);
+
+		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
+
+		assert_balance(account_id, EXISTENTIAL_DEPOSIT);
+		assert_balance(node_account, KITTY_PRICE);
 
 		System::assert_last_event(RuntimeEvent::KittiesModule(Event::KittyCreated {
 			who: account_id,
@@ -34,6 +49,7 @@ fn it_works_for_breed() {
 	new_test_ext().execute_with(|| {
 		let kitty_id = 0;
 		let account_id = 1;
+		let node_account = AccountIdConversion::<u64>::into_account_truncating(&PALLET_ID);
 
 		assert_noop!(
 			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id),
@@ -45,12 +61,20 @@ fn it_works_for_breed() {
 			Error::<Test>::InvalidKittyId
 		);
 
+		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE * 3 + EXISTENTIAL_DEPOSIT);
+
 		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
 		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
 
 		assert_eq!(KittiesModule::next_kitty_id(), kitty_id + 2);
 
+		assert_balance(account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
+		assert_balance(node_account, KITTY_PRICE * 2);
+
 		assert_ok!(KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id + 1));
+
+		assert_balance(account_id, EXISTENTIAL_DEPOSIT);
+		assert_balance(node_account, KITTY_PRICE * 3);
 
 		let breed_kitty_id = 2;
 
@@ -74,6 +98,7 @@ fn it_works_for_transfer() {
 		let account_id = 1;
 		let recipient = 2;
 
+		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
 
