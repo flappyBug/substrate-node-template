936162a14c55bbc489b672790bea69526af17455
Haoran Lin <tdkgn@outlook.com>
2023-06-09 09:09:56 +0800

    fix test

--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -7,6 +7,8 @@ fn assert_balance(account_id: u64, balance: u128) {
 	assert_eq!(Balances::total_balance(&account_id), balance);
 }
 
+const NAME: [u8; 8] = *b"TESTNAME";
+
 #[test]
 fn create_kitty_works() {
 	new_test_ext().execute_with(|| {
@@ -15,12 +17,12 @@ fn create_kitty_works() {
 		let node_account = AccountIdConversion::<u64>::into_account_truncating(&PALLET_ID);
 		assert_eq!(KittiesModule::next_kitty_id(), kitty_id);
 		assert_noop!(
-			KittiesModule::create(RuntimeOrigin::signed(account_id)),
+			KittiesModule::create(RuntimeOrigin::signed(account_id), NAME),
 			BalanceError::<Test>::InsufficientBalance
 		);
 
 		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id), NAME));
 
 		assert_balance(account_id, EXISTENTIAL_DEPOSIT);
 		assert_balance(node_account, KITTY_PRICE);
@@ -38,7 +40,7 @@ fn create_kitty_works() {
 
 		crate::NextKittyId::<Test>::set(crate::KittyId::max_value());
 		assert_noop!(
-			KittiesModule::create(RuntimeOrigin::signed(account_id)),
+			KittiesModule::create(RuntimeOrigin::signed(account_id), NAME),
 			Error::<Test>::InvalidKittyId
 		);
 	});
@@ -52,26 +54,31 @@ fn it_works_for_breed() {
 		let node_account = AccountIdConversion::<u64>::into_account_truncating(&PALLET_ID);
 
 		assert_noop!(
-			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id),
+			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id, NAME),
 			Error::<Test>::SameKittyId
 		);
 
 		assert_noop!(
-			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id + 1),
+			KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id + 1, NAME),
 			Error::<Test>::InvalidKittyId
 		);
 
 		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE * 3 + EXISTENTIAL_DEPOSIT);
 
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id), NAME));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id), NAME));
 
 		assert_eq!(KittiesModule::next_kitty_id(), kitty_id + 2);
 
 		assert_balance(account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 		assert_balance(node_account, KITTY_PRICE * 2);
 
-		assert_ok!(KittiesModule::breed(RuntimeOrigin::signed(account_id), kitty_id, kitty_id + 1));
+		assert_ok!(KittiesModule::breed(
+			RuntimeOrigin::signed(account_id),
+			kitty_id,
+			kitty_id + 1,
+			NAME
+		),);
 
 		assert_balance(account_id, EXISTENTIAL_DEPOSIT);
 		assert_balance(node_account, KITTY_PRICE * 3);
@@ -99,7 +106,7 @@ fn it_works_for_transfer() {
 		let recipient = 2;
 
 		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id), NAME));
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
 
 		assert_noop!(
@@ -140,7 +147,7 @@ fn it_works_for_sale() {
 
 		let _ = Balances::deposit_creating(&account_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id)));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(account_id), NAME));
 		assert_ok!(KittiesModule::sale(RuntimeOrigin::signed(account_id), kitty_id));
 		assert!(KittiesModule::kitty_on_sale(kitty_id).is_some());
 		assert_eq!(KittiesModule::kitty_owner(kitty_id), Some(account_id));
@@ -171,7 +178,7 @@ fn it_works_for_buy() {
 		let _ = Balances::deposit_creating(&seller_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 		let _ = Balances::deposit_creating(&buyer_id, KITTY_PRICE + EXISTENTIAL_DEPOSIT);
 
-		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(seller_id)));
+		assert_ok!(KittiesModule::create(RuntimeOrigin::signed(seller_id), NAME));
 		assert_ok!(KittiesModule::sale(RuntimeOrigin::signed(seller_id), kitty_id));
 
 		assert_ok!(KittiesModule::buy(RuntimeOrigin::signed(buyer_id,), kitty_id),);
